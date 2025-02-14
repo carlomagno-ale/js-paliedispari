c9c01c569ea8733fc909f0542ba02aa8fe18eb35
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-14 16:17:51 +0100

    add palidroma first step (in corso)

--- palidroma/script.js
+++ palidroma/script.js
@@ -1,2 +1,32 @@
 /*Palidroma
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma*/
+
+//utente inserisce la parola
+
+const userWord = prompt('Inserici una parola palidroma [es. osso, oro, afa, radar ...]');
+
+//dividiamo la parola in lettere
+
+let singleLetters = userWord.toLowerCase.split('');
+console.log(singleLetters);
+
+//identificare le lettere che formano la parola e leggerla al contrario
+
+singleLetters = singleLetters.reverse();
+
+const inverseWord = singleLetters.join('');
+
+console.log(singleLetters)
+console.log(inverseWord)
+
+//controllare che le due parole siano identiche
+
+if (userWord === inverseWord){
+    console.log('palidroma')
+} else {
+    console.log('no')
+}
+
+
+// creare la funzione per leggere al contrario
+
--- pari-dispari/script.js
+++ pari-dispari/script.js
@@ -2,5 +2,9 @@
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 */
 
-
+// utente sceglie pari/dispari
+// utente inserisce un numero da 1 a 5
+// PC inserisce un numero randomico da 1 a 5
+// sommiamo i due numeri e stabiliamo se è pari o dispari
+// dichiaro vincitore
 
