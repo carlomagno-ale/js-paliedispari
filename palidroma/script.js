6dcc37336f9233522b1c1deb1b2dcea330b0992f
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-14 17:08:29 +0100

    chore es palidroma

--- palidroma/script.js
+++ palidroma/script.js
@@ -1,48 +1,21 @@
 /*Palidroma
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma*/
 
-/*
-//utente inserisce la parola
+// utente inserisce la parola
+// dividiamo la parola in lettere 
+// identificare le lettere che formano la parola e leggerla al contrario
+// controllare che le due parole siano identiche
+// ! rendere gli uppercase identici durante il controllo
+
 
 const userWord = prompt('Inserici una parola palidroma [es. osso, oro, afa, radar ...]');
 
-//dividiamo la parola in lettere 
-
-let singleLetters = userWord.toLowerCase.split('');
-console.log(singleLetters);
-
-//identificare le lettere che formano la parola e leggerla al contrario
-
-singleLetters = singleLetters.reverse();
-
-const inverseWord = singleLetters.join('');
-
-console.log(singleLetters)
-console.log(inverseWord)
-
-//controllare che le due parole siano identiche
-
-if (userWord === inverseWord){
-    console.log('palidroma')
-} else {
-    console.log('no')
-}
-*/
-
-// creare la funzione per leggere al contrario
-
-
-let userWord = prompt('Inserici una parola palidroma [es. osso, oro, afa, radar ...]');
-
 function isPalidroma(word){
-
     const lowerWord = word.toLowerCase();
     const inverseWord = word.split('').reverse().join('').toLowerCase();
-
     if(lowerWord === inverseWord) {
         return true
     } 
-
     return false
   }
 
